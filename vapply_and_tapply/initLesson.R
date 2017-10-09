2e0122f56aad53c98197933a2808f8c0fceeb8c4
Simon Queenborough <simon.queenborough@yale.edu>
2017-10-08 22:38:42 -0400

    logging

--- vapply_and_tapply/initLesson.R
+++ vapply_and_tapply/initLesson.R
@@ -1,4 +1,8 @@
 # For compatibility with 2.2.21
+
+swirl_options(swirl_logging = TRUE)
+
+
 .get_course_path <- function(){
   tryCatch(swirl:::swirl_courses_dir(),
            error = function(c) {file.path(find.package("swirl"),"Courses")}
